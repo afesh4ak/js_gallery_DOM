e74487c807d47914e23e1ec698a0b7ebcaee7e09
Alina <afesh4ak@gmail.com>
2024-03-13 20:27:56 +0100

    make build

---
+++ index.a8d36631.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){var r=e.target.classList.contains("gallery__img"),l=e.target.classList.contains("list-item__link");if(r||l){e.preventDefault();var a=e.target.closest(".list-item__link").href;t.src=a}});//# sourceMappingURL=index.a8d36631.js.map
+
+//# sourceMappingURL=index.a8d36631.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const largeImg = document.querySelector("#largeImg");
+gallery.addEventListener("click", (clickEvent)=>{
+    const isGalleryImg = clickEvent.target.classList.contains("gallery__img");
+    const isItemLink = clickEvent.target.classList.contains("list-item__link");
+    if (isGalleryImg || isItemLink) {
+        clickEvent.preventDefault();
+        const imgURL = clickEvent.target.closest(".list-item__link").href;
+        largeImg.src = imgURL;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
